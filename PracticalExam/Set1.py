02772b686f7a161a53d5dc14e15dc095b370dcd1
Average Blank <hussainkhorakiwala45@outlook.com>
2023-12-06 23:41:50 +0530

    Update Set1.py
    Sorry Aaloke 2
    
    Co-Authored-By: Hemanth Tenneti <hemanth10etii@gmail.com>

--- PracticalExam/Set1.py
+++ PracticalExam/Set1.py
@@ -1,15 +1,43 @@
 ##
 # ! Pandas
+def printLines(a, b="sub heading"):
+    if b.lower() == "title" or b.lower() == "t":
+        print("-" * 80)
+        print(f"{'-' * 20} {a}")
+        print("-" * 80)
+    else:
+        print(f"{'-' * 10} {a}")
+
 
 import pandas as pd
 
+
+printLines("Pandas", "t")
+
+printLines("Creating Database")
 serObj = pd.Series([31, 28, 31, 30], index=["Jan", "Feb", "Mar", "Apr"])
 print(serObj)
+
+printLines("Adding May")
+serObj["May"] = 31
+print(serObj)
+
+printLines("Updating Feb")
 serObj["Feb"] = 29
 print(serObj)
-serObj.index = [1, 2, 3, 4]
+
+printLines("Replacing Index")
+serObj.index = [1, 2, 3, 4, 5]
 print(serObj)
+
+printLines("Printing month having less than 31 days")
 print([serObj < 31].index)
+
+printLines("V (a)")
+print(serObj < 30)
+
+printLines("V (b)")
+print(serObj + 3)
 
 
 # ! MatPlotLib
